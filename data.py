ce881479cf616c5114889a60cbc9e344b675f6fd
Hao Qiang <wind209@sina.com>
2018-12-16 11:55:57 +0800

    Update data.py

--- data.py
+++ data.py
@@ -20,13 +20,11 @@ def load_data(face_img_path, aus_pkl_path):
         aus_dict = pickle.load(f)
     imgs = np.zeros((len(imgs_names), 128, 128, 3), dtype=np.float32)
     aus = np.zeros((len(imgs_names), 17), dtype=np.float32)
-    #imgs = np.zeros((10000, 128, 128, 3), dtype=np.float32)
-    #aus = np.zeros((10000, 17), dtype=np.float32)
 
     for i, img_name in tqdm(enumerate(imgs_names)):
         img = cv2.imread(os.path.join(face_img_path, img_name))[:, :, ::-1]  # BGR -> RGB
         img = img / 127.5 - 1  # rescale within [-1,1]
         imgs[i] = img
-        aus[i] = aus_dict[img_name] / 5
+        aus[i] = aus_dict[img_name.split('.')[0]] / 5
 
     return imgs, aus

d5a7a6210c5c5cc7e7250d681615da95c9d51b80
Hao Qiang <wind209@sina.com>
2018-12-25 14:14:37 +0800

    Update aus_check.py

--- data/aus_check.py
+++ data/aus_check.py
@@ -3,6 +3,6 @@ import pickle
 with open('aus.pkl', 'rb') as f:
     aus = pickle.load(f)
 
-#print(aus)
-#print(list(aus.keys()))
+# print(aus)
+# print(list(aus.keys()))
 print(aus['N_0000000021_00562.jpg']/5)
